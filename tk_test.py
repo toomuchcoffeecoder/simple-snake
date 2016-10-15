15e122bd1d5adefe7640fe5d2cff8f91fa1156d3
Tim <Tim>
2016-10-15 15:47:20 -0500

    minor cleanup

--- tk_test.py
+++ tk_test.py
@@ -1,16 +1,19 @@
 from collections import deque
 import tkinter
 
-time1 = None
 x = 250
 y = 300
 xvect = 10
 yvect = 0
+last_xvect = None
+last_yvect = None
 line_len = 20
 line = deque()
 
 def key(event):
-    global xvect, yvect
+    global xvect, yvect, last_xvect, last_yvect
+    last_xvect = xvect
+    last_yvect = yvect
     if event.keysym == 'd':
         xvect = 10
         yvect = 0
@@ -24,8 +27,6 @@ def key(event):
         xvect = 0
         yvect = 10
 
-
-
 def draw():
     global x, y, xvect, yvect
     if len(line) == line_len:
@@ -35,8 +36,17 @@ def draw():
     x += xvect
     y += yvect
     point = [x,y]
+    if len(line) > 0:
+        if point == line[-1]:
+            x += last_xvect
+            y += last_yvect
+            xvect = last_xvect
+            yvect = last_yvect
+            point = [x,y]
+
     if point in line:
         quit()
+
     C.create_line(x, y, x + 10, y,fill="red", width=10)
     line.append(point)
     top.after(16, draw)
