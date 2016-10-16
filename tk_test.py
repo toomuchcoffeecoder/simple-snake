12b16e428966c1e30efb6cae9022a533660d6ada
Tim <Tim>
2016-10-16 12:17:58 -0500

    minor refactor and code that prevents snake from moving backward now works, added some comments

--- tk_test.py
+++ tk_test.py
@@ -10,7 +10,7 @@ ymove = 0
 last_xmove = None
 last_ymove = None
 snake_len = 20
-snake = deque()
+snake = deque([[x, y]])
 
 def key(event):
     global xmove, ymove, last_xmove, last_ymove
@@ -31,24 +31,21 @@ def key(event):
 
 def draw():
     global x, y, xmove, ymove
-    if len(snake) == snake_len:
+    if len(snake) == snake_len: # don't let the snake stretch longer than its length
         point = snake.popleft()
         C.create_line(point[0], point[1], point[0] + 10, point[1], fill=BG_COLOR, width=10)
 
-    x += xmove
-    y += ymove
-    point = [x,y]
-    if len(snake) > 0:
-        if point == snake[-1]:
-            x += last_xmove
-            y += last_ymove
+    point = [x + xmove, y + ymove]
+    if len(snake) > 1: # snakes dont move backwards
+        if point == snake[-2]:
             xmove = last_xmove
             ymove = last_ymove
-            point = [x,y]
-
-    if point in snake:
+            point = [x + last_xmove, y + last_ymove]
+        
+    if point in snake: # snake ate itself and died
         quit()
 
+    x, y = point
     C.create_line(x, y, x + 10, y,fill=FG_COLOR, width=10)
     snake.append(point)
     top.after(16, draw)
